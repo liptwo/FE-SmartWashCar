44aad20a8a0b13d31236b18dd72450f5683126fc
liptwo <142609541+liptwo@users.noreply.github.com>
2026-06-15 15:11:15 +0700

    fix: remove check isEmail

--- src/features/auth/services/auth-service.ts
+++ src/features/auth/services/auth-service.ts
@@ -39,18 +39,10 @@ export const authService = {
   },
 
   async login(payload: LoginPayload): Promise<AuthResponse> {
-    const isEmail = payload.emailOrPhone.includes('@')
-    const requestBody: any = {
+    const { data } = await authorizeAxios.post<AuthResponse>('/auth/login', {
+      emailOrPhone: payload.emailOrPhone,
       password: payload.password,
-    }
-
-    if (isEmail) {
-      requestBody.email = payload.emailOrPhone.trim()
-    } else {
-      requestBody.phone = payload.emailOrPhone.trim()
-    }
-
-    const { data } = await authorizeAxios.post<AuthResponse>('/auth/login', requestBody)
+    })
     return data
   },
 
